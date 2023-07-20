62056beac7849a521e4c62da42fd0702babe9c6a
anasselhaoussi <anous.elhaoussi@gmail.com>
2023-07-20 17:13:07 +0200

    feat : adding hovers for card buttons and changing colors

--- src/Pages/Pricing.tsx
+++ src/Pages/Pricing.tsx
@@ -8,7 +8,7 @@ const Pricing = () => {
       <div className="flex flex-col items-center gap-y-16 lg:w-[80%]">
         <div className="space-y-4">
           <div className="relative">
-            <h1 className="relative z-10 md:text-8xl sm:text-7xl text-6xl md:font-black font-extrabold">
+            <h1 className="relative z-10 md:text-8xl sm:text-7xl text-6xl md:font-black font-extrabold text-gray-200">
               Buy credits!
             </h1>
             <div className="absolute -z-0 h-32 w-full -top-10 bg-gray-800 blur-2xl"></div>
@@ -23,21 +23,21 @@ const Pricing = () => {
                 <h3 className="text-5xl font-bold text-blue-700">Free</h3>
                 <p className="text-sm font-light">Enjoy our free plan by using 10 credits.</p>
             </div>
-            <button className="py-3 px-8 bg-blue-700 rounded-md font-light whitespace-nowrap">Go to dashboard</button>
+            <button className="py-3 px-8 bg-blue-700 hover:scale-105 rounded-md font-light whitespace-nowrap transition-all">Go to dashboard</button>
           </div>
           <div className="flex flex-col gap-y-6 bg-gray-900 items-center justify-around p-8 rounded-2xl shadow-lg  md:w-[20rem] w-full">
             <div className="space-y-2">
                 <h3 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-purple-700">Basic</h3>
                 <p className="text-sm font-light">Buy 50 credits with 9$</p>
             </div>
-            <button className="py-3 px-5 bg-gradient-to-r from-blue-700 to-purple-700 rounded-md font-light">Buy in 10$</button>
+            <button className="py-3 px-5 bg-gradient-to-r hover:scale-105 from-blue-700 to-purple-700 rounded-md font-light transition-all">Buy in 10$</button>
           </div>
           <div className="flex flex-col gap-y-6 bg-gray-900 items-center justify-around p-8 rounded-2xl shadow-lg  md:w-[20rem] w-full">
             <div className="space-y-2">
                 <h3 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-purple-700">Premium</h3>
                 <p className="text-sm font-light">Enjoy our premium plan by buying 200 credits in only 20$</p>
             </div>
-            <button className="py-3 px-5 bg-gradient-to-r from-blue-700 to-purple-700 rounded-md font-light">Buy in 20$</button>
+            <button className="py-3 px-5 bg-gradient-to-r hover:scale-105 from-blue-700 to-purple-700 rounded-md font-light transition-all">Buy in 20$</button>
           </div>
         </div>
       </div>
