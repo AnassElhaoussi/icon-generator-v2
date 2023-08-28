aeedc556b74ac92e8fe8057dba6291009d21b072
anasselhaoussi <anous.elhaoussi@gmail.com>
2023-08-28 12:14:35 +0200

    feat : state that keeps track of the style of icons

--- client/src/components/dashboard/IconStyles.tsx
+++ client/src/components/dashboard/IconStyles.tsx
@@ -1,24 +1,68 @@
-import React from "react";
-import { FormControl, FormLabel, Flex, Card, CardHeader, Heading, Image, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  FormControl,
+  FormLabel,
+  Flex,
+  Card,
+  CardHeader,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import { iconStyles } from "../../constants/iconstyles";
 import { DarkMode } from "@chakra-ui/react";
+import { easeIn } from "framer-motion";
 
 const IconStyles = () => {
+  const [chosenStyle, setChosenStyle] = useState<string>("");
+
+  const chooseStyle = (name: string) => {
+    setChosenStyle(name);
+  };
   return (
     <DarkMode>
-        <FormControl>
-            <FormLabel fontSize="lg" className="bg-gradient-to-r from-gray-400 to-white text-transparent bg-clip-text">Choose your icon style</FormLabel>
-            <Flex display="flex" flexWrap="wrap" rowGap="1rem" columnGap="2rem">
-                {iconStyles.map(({name, description, imgUrl}) => (
-                    <Card backgroundColor="#1A1A1A" className="hover:scale-110 transition-all bg-gradient-to-t" dropShadow="dark-lg" cursor="pointer" borderRadius="2xl">
-                        <CardHeader>
-                            <Heading fontFamily="Poppins, sans-serif" fontWeight="normal" fontSize="2rem" textColor="gray.200">{name}</Heading>
-                            {description && <Text textColor="gray.600" fontSize="sm">{description}</Text>}
-                        </CardHeader>
-                    </Card>
-                ))}
-            </Flex>
-        </FormControl>
+      <FormControl>
+        <FormLabel
+          fontSize="lg"
+          className="bg-gradient-to-r from-gray-400 to-white text-transparent bg-clip-text"
+        >
+          Choose your icon style
+        </FormLabel>
+        <Flex display="flex" flexWrap="wrap" rowGap="1rem" columnGap="2rem" alignItems="stretch">
+          {iconStyles.map(({ name, description, imgUrl }) => (
+            <Card
+              onClick={() => chooseStyle(name)}
+              backgroundColor="#1A1A1A"
+              dropShadow="dark-lg"
+              cursor="pointer"
+              borderRadius="2xl"
+              className="transition-all"
+              style={{
+                borderBottom:
+                  chosenStyle === name
+                    ? "8px solid blueviolet"
+                    : "2px solid transparent",
+              }}
+            >
+              <CardHeader>
+                <Heading
+                  fontFamily="Poppins, sans-serif"
+                  fontWeight="normal"
+                  fontSize="2rem"
+                  textColor="gray.200"
+                >
+                  {name}
+                </Heading>
+                {description && (
+                  <Text textColor="gray.600" fontSize="sm">
+                    {description}
+                  </Text>
+                )}
+              </CardHeader>
+            </Card>
+          ))}
+        </Flex>
+      </FormControl>
     </DarkMode>
   );
 };
